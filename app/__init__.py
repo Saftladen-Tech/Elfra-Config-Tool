3f4db0cc19545b2b49f8f788a28f44786a9c64f7
LumaKEKW <lucasmentel1909@gmail.com>
2025-08-23 10:07:53 +0200

    Added package

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 csrf = CSRFProtect(app)
 
 from app.routes import index
